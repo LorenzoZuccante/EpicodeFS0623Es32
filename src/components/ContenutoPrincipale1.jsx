8fdd73b4ee8eaed4efc88bafa1f16667c19633ee
Lorenzo <lorenzo.zuccante@outlook.it>
2024-01-08 15:20:32 +0100

    esercizio 36

--- src/components/ContenutoPrincipale1.jsx
+++ src/components/ContenutoPrincipale1.jsx
@@ -7,13 +7,12 @@ import CommentsSection from "./CommentsSection";
 const SingleBook = ({ libro, isSelected, handleClick }) => {
   return (
     <div className={`col-md-4 col-sm-6 col-lg-3 p-2`} onClick={() => handleClick(libro.asin)}>
-      <Card style={{ width: "13rem" }} className={isSelected ? 'border border-danger' : ''}>
+      <Card style={{ width: "8rem" }} className={isSelected ? 'border border-danger' : ''}>
         <Card.Img variant="top" src={libro.img} />
         <Card.Body>
           <Card.Text>{libro.title}</Card.Text>
         </Card.Body>
       </Card>
-      {isSelected && <CommentsSection elementId={libro.asin} />}
     </div>
   );
 };
@@ -41,43 +40,52 @@ const ContenutoPrincipale1 = () => {
         );
 
   return (
-    <div className="container">
-      <div className="my-4">
-        <Form>
-          <Form.Group controlId="formSearch">
-            <Form.Label>Cerca per titolo</Form.Label>
-            <Form.Control
-              type="text"
-              placeholder="Inserisci il titolo del libro"
-              value={searchValue}
-              onChange={(e) => setSearchValue(e.target.value)}
-            />
-          </Form.Group>
-          <Form.Group controlId="formCategory">
-            <Form.Label>Scegli la categoria</Form.Label>
-            <Form.Control
-              as="select"
-              value={selectedCategory}
-              onChange={(e) => setSelectedCategory(e.target.value)}
-            >
-              {allCategories.map((category, index) => (
-                <option key={index} value={category}>
-                  {category}
-                </option>
-              ))}
-            </Form.Control>
-          </Form.Group>
-        </Form>
-      </div>
+    <div className="container-fluid">
       <div className="row">
-        {filteredBooks.map((libro, index) => (
-          <SingleBook
-            key={index}
-            libro={libro}
-            isSelected={libro.asin === selectedBook}
-            handleClick={handleCardClick}
-          />
-        ))}
+        <div className="col-md-6">
+          <div className="my-4">
+            <Form>
+              <Form.Group controlId="formSearch">
+                <Form.Label>Cerca per titolo</Form.Label>
+                <Form.Control
+                  type="text"
+                  placeholder="Inserisci il titolo del libro"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
+                />
+              </Form.Group>
+              <Form.Group controlId="formCategory">
+                <Form.Label>Scegli la categoria</Form.Label>
+                <Form.Control
+                  as="select"
+                  value={selectedCategory}
+                  onChange={(e) => setSelectedCategory(e.target.value)}
+                >
+                  {allCategories.map((category, index) => (
+                    <option key={index} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </Form.Control>
+              </Form.Group>
+            </Form>
+          </div>
+          <div className="row">
+            {filteredBooks.map((libro, index) => (
+              <SingleBook
+                key={index}
+                libro={libro}
+                isSelected={libro.asin === selectedBook}
+                handleClick={handleCardClick}
+              />
+            ))}
+          </div>
+        </div>
+        {selectedBook && (
+          <div className="col-md-6">
+            <CommentsSection elementId={selectedBook} />
+          </div>
+        )}
       </div>
     </div>
   );
